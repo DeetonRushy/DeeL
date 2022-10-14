26317449a5e057f8d2ed1e05bd304420d17bb18b
Deeton Rushton <deetonrushton1@outlook.com>
2022-10-14 17:00:15 +0100

    New lexer constants
    Add CallOpen and CallClose.
    
    These characters represent the start and end of a function call

--- DL/Lexer/DConstants.cs
+++ DL/Lexer/DConstants.cs
@@ -6,15 +6,19 @@ public static class DConstants
 {
     public const char EOF = '\0';
 
-    public const char DictOpen = '{';
-    public const char DictClose = '}';
+    public const char DictOpen =   '{';
+    public const char DictClose =  '}';
 
-    public const char Comma = ',';
+    public const char Comma =      ',';
     public new const char Equals = '=';
 
-    public const char ListOpen = '[';
-    public const char ListClose = ']';
-    public const char Colon = ':';
+    public const char ListOpen =   '[';
+    public const char ListClose =  ']';
+
+    public const char CallOpen =   '(';
+    public const char CallClose =  ')';
+
+    public const char Colon =      ':';
 
     public static readonly List<char> StringDelims = new() { '\'', '"' };
 
@@ -27,13 +31,13 @@ public static class DConstants
     /// Attempt to work this into the source, in a way that would allow this to be changed to '\n'
     /// without many problems.
     /// </summary>
-    public const char Endline = '\n';
-    public const char LineBreak = ';';
+    public const char Endline =        '\n';
+    public const char LineBreak =      ';';
 
     public const char WindowsGarbage = '\r';
-    public const char Whitespace = ' ';
+    public const char Whitespace =     ' ';
 
-    public const char Comment = '#';
+    public const char Comment =        '#';
 
     public static readonly Dictionary<string, TokenType> SpecialRhsAssignees = new()
     {
