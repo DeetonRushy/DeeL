63a5b6a2bec75bd3cf5ae291157c89f46bc36b60
deeton rushy <deetonrushton1@outlook.com>
2022-11-27 22:37:04 +0000

    Add a goal script + test our interop
    The interop currently returns the native C# function declaration. This is to be wrapped with a DL type that can be called. (maybe UserDefinedFunction?)

--- playground/Program.cs
+++ playground/Program.cs
@@ -1,17 +1,61 @@
 ﻿using Runtime;
 using System.Diagnostics;
 
-DContext ctx = null!;
+DContext ctx;
 
 try
 {
     var sw = Stopwatch.StartNew();
-    var source = @"
-mod 'DL';
+    var goal = @"
+from 'std.io' import { File, Result, Okay, Failure };
 
-let value: integer = interop::get_native_function('System.Environment', 'GetEnvironmentVariable');
+fn openFile(name: string) -> Result<File> {
+  let result: Result<> = File::open(name);
+
+  if (result.ok()) {
+    return Okay(result::value());
+  }
+
+  return Failure(result::error());
+}
+
+from 'std' import { Env };
+from 'std.ext' import { ArgParser, Namespace, quit };
+
+let argv: list<string> = Env::argv();
+let parser: ArgParser = ArgParser::new(argv);
+
+parser::add_required('n', 'name', {'desc'='the file to read the contents of'});
+parser::add_optional('v', 'verbose', {'desc'='enable verbose mode'});
+
+let results: Namespace = parser::compute({'capture-errors': true});
+if (results::errors::size() != 0) {
+  writeln(@'errors: {results::errors}');
+  quit(-1);
+}
+
+let path: string = results::name;
+let handle = openFile(path);
+
+if (!path::ok()) {
+  writeln(@'failed to discover file: {handle::error()::message}');
+  quit(-2);
+}
+
+for (let line in iterate(handle::value()::read_lines())) {
+  writeln(@'{line}\n');
+}
+
+if (results::verbose) {
+  writeln(@'**** iterated {handle::value()::count} lines ****');
+}
+
 ";
-    ctx = DlRuntime.Run(source, false);
+
+    var new_source = @"
+let b: any = CSharp::get_native_function('Runtime.Configuration', 'RegisterDefaultFlag');
+";
+    ctx = DlRuntime.Run(new_source, false);
 
     sw.Stop();
     Console.WriteLine($"\n\nTotal execution time: {sw.Elapsed} ({sw.ElapsedMilliseconds}ms)");
