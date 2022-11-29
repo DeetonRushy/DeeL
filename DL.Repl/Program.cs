44b1ff59e9dd9791d7be8f41f124fb222159592e
deeton rushy <deetonrushton1@outlook.com>
2022-11-29 02:49:54 +0000

    wtf
    not sure why this wasnt added to git when I physically remember doing it

---
+++ DL.Repl/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * This is the very start of what I hope to be a pretty cool REPL.
+ *
+ * Due to the language's nature, we'll easily be able to extend it with functions related to
+ * the REPL.
+ *
+ * Such as:
+ *   Setting the languages configuration in real-time from the language itself.
+ *
+ * Not going to write too much, as it's not even turing complete yet.
+ */
+
+using DL.Repl.Application;
+
+new Repl().Execute();
